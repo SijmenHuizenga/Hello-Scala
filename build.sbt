3e151efb1eddee89d6e27c41b6c245a47593bd2f
sijmenhuizenga <sijmenhuizenga@gmail.com>
2017-03-24 16:08:46 +0100

    Day 1; first things first

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "Hello-Scala"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ src/main/scala/hello.scala
@@ -0,0 +1,48 @@
+/**
+ * Created by Sijmen on 24-3-2017.
+ */
+val a = 2
+val b = 2
+
+if (b < a) {
+  println("true")
+} else {
+  println("false")
+}
+
+def whileLoop: Unit = {
+  var i = 1
+  println( "While Loop" )
+  while(i <= 10) {
+    println(i)
+    i+=1
+  }
+}
+whileLoop
+
+def forLoop(args: Range) {
+  println( "for loop " )
+  for(i <- 0 until args.length)
+    println(args(i))
+}
+forNiceLoop(0 until 5)
+def forNiceLoop(args: Range) {
+  println( "for loop nice way" )
+  args.foreach { arg =>
+    println(arg)
+  }
+}
+var range05 = (0 until 5)
+forNiceLoop(range05)
+
+def forCoolLoop(args: Range) {
+  println( "for loop even better way" )
+  args.foreach { println(_) }
+}
+forNiceLoop(0 until 5)
+
+val familie = ("ik", "broertje", "papa", "mama")
+val c= familie._1
+
+val (ik, broertje, papa, mama) = familie
+println(ik);
